40b464a6c6504a152861a738ad5d8883e812784b
Carlos Ernesto Diaz Acosta <85591130+cediazz@users.noreply.github.com>
2024-11-17 17:47:55 -0500

    Update forms.py

--- repository/forms.py
+++ repository/forms.py
@@ -5,7 +5,7 @@ class DocumentForm(forms.ModelForm):
     class Meta():
         model = Document
         fields = ['title','description','author','theme','file']
-        widgets = {  # Se puede usar tambien la libreria django-widget-tweaks
+        widgets = {  
             'title': forms.TextInput(attrs={'class': 'form-control', 'id': 'title'}),
             'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3, 'id': 'description'}),
             'author': forms.TextInput(attrs={'class': 'form-control', 'id': 'author'}),
