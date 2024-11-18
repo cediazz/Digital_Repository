3fd1d6a3b29918ae7e3be5673bfa8f6a8ca7a96a
Carlos Ernesto Diaz Acosta <85591130+cediazz@users.noreply.github.com>
2024-11-17 19:20:08 -0500

    Update document_update_view.py

--- repository/views/document_update_view.py
+++ repository/views/document_update_view.py
@@ -3,9 +3,8 @@ from django.urls import reverse,reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from ..models import Document
 from ..forms import DocumentForm
-from django.utils.decorators import method_decorator
-from security.Utils import admin_restriction
-@method_decorator(admin_restriction, name='dispatch')
+
+
 class DocumentUpdateView(LoginRequiredMixin,UpdateView):
     model = Document
     form_class = DocumentForm
@@ -15,9 +14,4 @@ class DocumentUpdateView(LoginRequiredMixin,UpdateView):
     
     def get_success_url(self):
         return reverse_lazy('document-update', kwargs={'pk': self.object.pk})
-        """Al utilizar reverse_lazy(), se pospone la resolución de la URL hasta que sea necesaria, 
-           lo que puede ser útil en este contexto. Esto significa que la URL no se resolverá 
-           inmediatamente cuando se defina la vista, sino que se resolverá cuando sea necesario, por 
-           ejemplo, después de que el usuario actualice un documento y se requiera redirigirlo a la 
-           página de actualizacion del documento.El método reverse_lazy, en este caso, devolverá la 
-           URL respectiva para la vista 'document-update' con el parámetro 'pk' del objeto actual."""
+        
